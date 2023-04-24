ed4bfbbb557456108d698d8036b7f78daecfc5dc
Austin Aluzzi <mrfrozenfirefly@gmail.com>
2023-04-23 20:45:10 -0700

    Calculate total milliseconds instead of hardcoding it (in case of leap year)

--- src/AgeDisplay.js
+++ src/AgeDisplay.js
@@ -5,7 +5,7 @@ function AgeDisplay(prop) {
     const [age, setAge] = useState(0);
 
     useEffect(() => {
-        const id = setInterval(() => setAge(getExactAge(prop.birthday, new Date())), 10);
+        const id = setInterval(() => setAge(getYearsOld(prop.birthday, new Date())), 10);
         return () => clearInterval(id);  
       }, [prop.birthday]);
 
@@ -21,21 +21,16 @@ function AgeDisplay(prop) {
     ); 
 }
 
-const MILLI_IN_YEAR = 31_536_000_000;
-
-function getExactAge(birthday, now) {
-    return ((getYearsOld(birthday, now) + getPercentageToBirthdayThisYear(birthday, now)) + "000000000").slice(0, 12);
+function getYearsOld(birthday, now) {
+    return ((now.getFullYear() - birthday.getFullYear() - 1) + getPercentageToBirthdayThisYear(birthday, now)).toFixed(9);
 }
 
 //if birthday passed this year, percentage will be > 1
 function getPercentageToBirthdayThisYear(birthday, now) {
-    let birthdayLastYear = new Date(birthday.valueOf());
-    birthdayLastYear.setFullYear(now.getFullYear() - 1);
-    return (now - birthdayLastYear) / MILLI_IN_YEAR;
-}
-
-function getYearsOld(birthday, now) {
-    return now.getFullYear() - birthday.getFullYear() - 1;
+    let birthdayLastYear = new Date(birthday.valueOf()).setFullYear(now.getFullYear() - 1);
+    let birthdayThisYear = new Date(birthday.valueOf()).setFullYear(now.getFullYear());
+    
+    return (now - birthdayLastYear) / (birthdayThisYear - birthdayLastYear); //accounts for leap years
 }
 
 export default AgeDisplay;
