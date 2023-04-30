c5c2442b07c93cd8a87c1e214dc2157268e0d814
Austin Aluzzi <mrfrozenfirefly@gmail.com>
2023-04-29 20:19:44 -0700

    Implement themes savable through color search parameter

--- src/AgeForm.js
+++ src/AgeForm.js
@@ -1,4 +1,4 @@
-function AgeForm() {
+function AgeForm(prop) {
     return (
         <div className="content">
                 <div className="title">Age Calculator</div>
@@ -15,6 +15,14 @@ function AgeForm() {
                         <label htmlFor="name">Your Name (Optional)</label>
                         <input name="name" type="text" maxLength="22"/>
                     </div>
+                    <div>
+                        <label htmlFor="color">Theme</label>
+                        <select name="color" onChange={(e) => prop.changeTheme(e.target.value)}>
+                            {prop.themes.map((theme, index) => {
+                                return (<option value={index}>{theme}</option>);
+                            })}
+                        </select>
+                    </div>
                     <button type="submit">View Age</button>
                 </form>              
         </div>
@@ -25,6 +33,9 @@ function setSearchParameters(e) {
     e.preventDefault();
 
     let here = new URL(document.location.href);
+    if (e.target.elements.color.value !== "0") {
+        here.searchParams.set("color", e.target.elements.color.value);
+    }
     if (e.target.elements.name.value) {
         here.searchParams.set("name", e.target.elements.name.value);
     }
--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import AgeDisplay from './AgeDisplay';
 import AgeForm from './AgeForm';
+import { useState } from 'react';
+
+const themes = ["Blue", "Green", "Orange", "Pink", "Purple", "Red"];
 
 function App() {
   const query = new URLSearchParams(window.location.search);
   const birthday = query.get("birthday") ? new Date(query.get("birthday") + (query.get("birthday").includes("T") ? "" : "T00:00")) : null;
   const name = query.get("name");
+  const [theme, setTheme] = useState((themes[query.get("color")] ? themes[query.get("color")].toLowerCase() : "blue") + "-theme");
+
 
   if (birthday === null || isNaN(birthday) || birthday > new Date()) {
     return (
-      <div className="App">
-        <AgeForm />
+      <div className={"App " + theme}>
+        <AgeForm themes={themes} changeTheme={(index => {
+          setTheme(themes[index].toLowerCase() + "-theme")
+        })}/>
       </div>
     );
   }
   return (
-    <div className="App">
+    <div className={"App " + theme}>
         <AgeDisplay birthday={birthday} name={name}/>
     </div>
   );
