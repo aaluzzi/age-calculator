70c3b6f53d7fd8381f9ea44219903e8187ee92c1
Austin Aluzzi <mrfrozenfirefly@gmail.com>
2023-05-01 23:16:04 -0700

    Add tick animation to percentage text

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -3,8 +3,8 @@ function ProgressBar(prop) {
         <div className="progress" style={{"--progress": Math.floor(prop.percentage * 360) + "deg"}}>
             <div key={prop.measurement} className="circle-percentage">
                 <div className="percentage">
-                    <div>{Math.floor(prop.percentage * 10000) / 100}%</div>
-                    <div>to {prop.age + 1}</div>
+                    <div key={Math.floor(prop.percentage * 10000) / 100}>{Math.floor(prop.percentage * 10000) / 100}%</div>
+                    <div key={prop.age + 1}>to {prop.age + 1}</div>
                 </div>
             </div>  
         </div>
